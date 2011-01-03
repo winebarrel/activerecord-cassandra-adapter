c76fa3255cf85db1cf5bfc67c69c2c3e43986af6
winebarrel <sgwr_dts@yahoo.co.jp>
2011-01-04 01:44:32 +0900

    remove files. I leave github

--- package.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-VERSION=0.1.0
-
-rm *.gem *.tar.bz2
-rm -rf doc
-rdoc  -m README.rdoc README.rdoc --title 'activerecord-cassandra-adapter is a Cassandra adapter for ActiveRecord.'
-tar jcvf activerecord-cassandra-adapter-${VERSION}.tar.bz2 --exclude=.svn lib README *.gemspec doc
-gem build activerecord-cassandra-adapter.gemspec
